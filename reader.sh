71c22c4c04bf7136c0de9db61f5deabb6467deca
newaccountnewlife <no@p.e>
2020-09-25 21:31:50 +0200

    too many changes to list #2, works okish

--- reader.sh
+++ reader.sh
@@ -6,7 +6,7 @@ clientversion="0.2"
 server="104.131.93.82"
 port="27017"
 
-# read the official server from the terminal
+# use AO from the terminal
 # shitty
 # you will eventually be thrown out, likely
 # not a full client yet
@@ -17,6 +17,10 @@ port="27017"
 # make it a full client
 # make everything a function
 # implement all packets
+# implement master server
+# store the "hdid" in a file
+# 
+# 
 # 
 # DONE:
 # use a socket rather than nc
@@ -33,11 +37,11 @@ port="27017"
 # S: SC
 # S: SM
 # S: DONE
+# S: PV (?)
 # 
 # 
 # 
 # Packets yet to implement:
-# S: PV
 # S: CharsCheck
 # S: MS
 # S: BN
@@ -51,6 +55,10 @@ port="27017"
 # S: ARUP
 # S: LE
 # S: ZZ
+# S: KK
+# S: KB
+# S: BD
+# S: CHECK
 # 
 # 
 # C: CC
@@ -68,6 +76,7 @@ port="27017"
 # C: SETCASE
 # C: CASEA
 # C: ZZ
+# C: CH
 # 
 # 
 # etc. pp.
@@ -81,7 +90,7 @@ die() {
 }
 
 output() {
-	# output to the user here
+	echo "$1"
 }
 
 ###PROGRAM FUNCTIONS###
@@ -94,62 +103,162 @@ sendpacket() {
     	
 }
 
+###PACKET FUNCTIONS###
+
+SV_packet_PN() {
+    output 'Players/Max: '"$(awk -F# '{print $2"/"$3}' <<<"$1")"
+	
+}
+SV_packet_FL() {
+    output 'Features: '"$(tr '#' ', ' <<<"$1")"
+	
+}
+SV_packet_SC() {
+    output 'Characters available: '"$(tr '#' ', ' <<<"$1" | tr -d '&')"
+	
+}
+SV_packet_SM() {
+    output 'Available Music: '"$(tr '#' ', ' <<<"$1")"
+	
+}
+SV_packet_DONE() {
+    output 'Server Done.'
+	
+}
+
+SV_packet_PV() {
+    output 'Server changed your character.'
+	
+}
+SV_packet_CHECK() {
+	output 'The connection is still alive.'
+}
+SV_packet_XX() {
+	return
+}
+
+SV_packet_XX() {
+	return
+	
+}
+SV_packet_XX() {
+	
+	return
+}
+SV_packet_XX() {
+	
+	return
+}
+
+SV_packet_XX() {
+	return
+	
+}
+SV_packet_XX() {
+	
+	return
+}
+SV_packet_XX() {
+	return
+	
+}
+
+SV_packet_XX() {
+	return
+	
+}
+SV_packet_XX() {
+	
+	return
+}
+SV_packet_XX() {
+	return
+	
+}
+
+CL_packet_XX() {
+	return
+	
+}
+CL_packet_CH() {
+	sendpacket "CH#%"
+	
+}
+
+###PACKET FUNCTIONS###
+
+SV_debugpacket() {
+	output '<~'"$(awk -F# '{print $1}' <<<"$1")"
+}
+
+CL_debugpacket() {
+	output '~>'"$(awk -F# '{print $1}' <<<"$1")"
+}
+
 handlepacket() {
     # this handles incoming packets
     #
-    if grep -E '^PN' <<<"$1"; then
-    output 'Players/Max: '"$(awk -F# '{print $1"/"$2}')"
-    else
-    if grep -E '^FL' <<<"$1"; then
-    output 'Features: '"$(tr '#' ', ' <<<"$1")"
-    else
-    if grep -E '^SC' <<<"$1"; then
-    output 'Characters available: '"$(tr '#' ', ' <<<"$1" | tr -d '&')"
-    else
-    if grep -E '^SM' <<<"$1"; then
-    output 'Available Music: '"$(tr '#' ', ' <<<"$1")"
-    else
-    if grep -E '^DONE' <<<"$1"; then
-    output 'Server Done.'
-    else
-    if grep -E '^SM' <<<"$1"; then
-    else
-    if grep -E '^SC' <<<"$1"; then
-    else
-    if grep -E '^SM' <<<"$1"; then
-    else
-	
+      if grep -q -E '^PN' <<<"$1"; then
+         SV_packet_PN "$1"
+
+    elif grep -q -E '^FL' <<<"$1"; then
+         SV_packet_FL "$1"
+
+    elif grep -q -E '^SC' <<<"$1"; then
+         SV_packet_SC "$1"
+ 
+    elif grep -q -E '^SM' <<<"$1"; then
+         SV_packet_SM "$1"
+
+    elif grep -q -E '^DONE' <<<"$1"; then
+         SV_packet_DONE "$1"
+
+    elif grep -q -E '^CHECK' <<<"$1"; then
+         SV_packet_CHECK
+
+    elif grep -q -E '^XX' <<<"$1"; then
+         SV_packet_XX "$1"
+
+    elif grep -q -E '^XX' <<<"$1"; then
+         SV_packet_XX "$1"
+    
+    fi
 }
 
 
 handshake() {
 # handshake
-
 randomhdidlength="30"
-hdid="$(cat /dev/random | tr -dc 'a-f0-9' | fold -w ${randomhdidlength})"
+hdid="abcdef"
+# hdid="$(cat /dev/urandom | tr -dc 'a-f0-9' | fold -w ${randomhdidlength})"
 handshakestring='HI#'"${hdid}"'#%ID#'"${clientname}"'#'"${clientversion}"'#%RD#%'
-sendpacket "${handshakestring}"
+sendpacket "${handshakestring}" && output "Successful handshake with the server!"
 
 }
 
+connect() {
+    exec 3<>/dev/tcp/"${server}/${port}" || die "The connection to the server could not be established. Please check your network and the IP and port of the server."
+}
 ###NETWORKING FUNCTIONS###
+###MAIN###
 
-###PACKET FUNCTIONS###
+output "Connecting..."
+connect
+output "Shaking hands..."
+handshake
 
-###PACKET FUNCTIONS###
+while read -rd'%' -n 2048 -t10 packet ;do
+    
+    # handle incoming packets
+    handlepacket "${packet}"
+done <&3
 
-exec 3<>/dev/tcp/"${server}/${port}" || die "The connection to the server could not be established. Please check your network and the IP and port of the server."
-
-
-while read -r packet;do
-
-# handle incoming packets
-
-done < <(tr '%' '\n' <&3)
-
+# ignore dis
 # pi=""
 # while :;do
     # i="$( <&3 | tr '%' '\n' | tail -1 | grep -E '^MS' | awk -F'#' '{print $4"("$5"): "$6}')"
     # [[ "$pi" != "$i" && "$i" != "" ]] &&
     # echo "$i";pi="$i"
 # done
+
+###MAIN###
